1272916b046a47c7be50d7a775c66b932181742b
fjl <fengjianglin6817@dingtalk.com>
2018-06-15 17:15:34 +0800

    update

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 let mongoose=require('mongoose');
 let dbs=mongoose.createConnection('mongodb://47.106.13.245:27017/node');
-dbs.on('open',function (callback) {
+dbs.on('open', (callback)=> {
     console.log('数据库链接成功');
 });
 module.exports=dbs;
--- myapp.js
+++ myapp.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 let Student=require('./Students');
-let lisi=new Student({"name":"李四","age":23,"sex":"男"});
+let lisi=new Student({"username":"李四","age":23,"sex":"男"});
 lisi.save((err,res)=>{
    if (!err){
        console.log('成功');
